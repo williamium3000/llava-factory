31da4b9a4667c4f0a871939fb7533cacc3e9428a
william <liyijiang3000@gmail.com>
2025-05-20 02:00:59 +0000

    update script

--- lmms-eval/run.sh
+++ lmms-eval/run.sh
@@ -1,22 +1,23 @@
-accelerate launch \
-    --num_processes=4 \
-    -m lmms_eval \
-    --model tinyllava \
-    --model_args pretrained=/myfiles/project/llava-factory/work_dirs/llava-Qwen3-8B-clip-vit-large-patch14-336-base-finetune,conv_mode=qwen2_instruct \
-    --tasks mme,vqav2,gqa,scienceqa_img,textvqa,mmvet,pope,mme,mmmu_val \
-    --batch_size 1 \
-    --log_samples \
-    --log_samples_suffix llava-Qwen3-8B-clip-vit-large-patch14-336-base-finetune \
-    --output_path ./logs/
+# accelerate launch \
+#     --num_processes=4 \
+#     -m lmms_eval \
+#     --model tinyllava \
+#     --model_args pretrained=/myfiles/project/llava-factory/work_dirs/llava-Qwen3-8B-clip-vit-large-patch14-336-base-finetune,conv_mode=qwen2_instruct \
+#     --tasks mme,vqav2,gqa,scienceqa_img,textvqa,mmvet,pope,mme,mmmu_val \
+#     --batch_size 1 \
+#     --log_samples \
+#     --log_samples_suffix llava-Qwen3-8B-clip-vit-large-patch14-336-base-finetune \
+#     --output_path ./logs/
 
 
 accelerate launch \
     --num_processes=4 \
+    --main_process_port 29502 \
     -m lmms_eval \
-    --model tinyllava \
-    --model_args pretrained=work_dirs/llava-vicuna-7b-v1.5-clip-vit-large-patch14-336-base-finetune,conv_mode=llama \
-    --tasks mme,vqav2,gqa,scienceqa_img,textvqa,mmvet,pope,mme,mmmu_val \
+    --model llava   \
+    --model_args pretrained="liuhaotian/llava-v1.5-7b,device_map=auto" \
+    --tasks vqav2,gqa \
     --batch_size 1 \
     --log_samples \
-    --log_samples_suffix llava-vicuna-7b-v1.5-clip-vit-large-patch14-336-base-finetune \
+    --log_samples_suffix llava-v1.5-7b \
     --output_path ./logs/
